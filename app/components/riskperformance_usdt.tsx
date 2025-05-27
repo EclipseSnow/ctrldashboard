1a7667a53dadd1f42f0f6b5b3f9d489751efc5a7
EclipseSnow <damienhew@hotmail.com>
2025-05-27 11:48:19 +0800

    Update riskperformance_usdt.tsx

--- app/components/riskperformance_usdt.tsx
+++ app/components/riskperformance_usdt.tsx
@@ -61,7 +61,8 @@ export async function calculateNAVMetrics() {
     period_pnl: pnl.toFixed(2),
     period_pnl_percent: pnlPercent.toFixed(2),
     max_drawdown: maxDrawdown.toFixed(2),
-    annualized_return_1Y: annualizedReturn_1Y.toFixed(4)
+    annualized_return_1Y: annualizedReturn_1Y.toFixed(4),
+    inceptionDate: inceptionDate.toISOString().split('T')[0]
   };
 }
 
